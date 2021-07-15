128e529b5c5e45ce5e809cf12dc98b3f4a457bd3
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-15 17:19:47 +0200

    condizioni while

--- script.js
+++ script.js
@@ -11,57 +11,62 @@ Quando la partita termina, comunichiamo all'utente il suo punteggio.
 */
 
 
-/* 
-1. creo un array vuoto per contenere le bombe; ok
-2. come genero le bombe? Con math.random; da 1 a 100;     ok
-3. come le inserisco nell'array? con push;     ok 
-4. bisogna però chiedergli di fare questa cosa del random finchè non inserisce 16 numeri nell'array; ok
-5.  Controllo di non inserire random uguali.
-*/ 
-
-var bombs = [];                                         /* per memorizzare le bombe */ 
-var bombsNumbers = 6;                        /*(16)     /* lunghezza dell'array bombe */
-var maxNumbers = 10;                      /*(100)       /* tetto massimo numerico */
-var userChance = maxNumbers - bombsNumbers;             /* le chance totali dell'utente */ 
-var userNumbers = [];                                    /* per memorizzare i numeri scelti dall'utente */
-var userChoice;                                         /* per chiedere i numeri all'utente */
-var isGameOver = false;                                 /* per interrompere il ciclo*/
+var bombs = [];                                             /* per memorizzare le bombe */ 
+var bombsNumbers = 6;     /*(16)*/   /modificare/           /* lunghezza dell'array bombe */
+var maxNumbers = 10;      /*(100)*/  /modificare/           /* tetto massimo numerico */
+var userChance = maxNumbers - bombsNumbers;                 /* le chance totali dell'utente */ 
+var userNumbers = [];                                       /* per memorizzare i numeri scelti dall'utente */
+var isGameOver = false;                                     /* per interrompere il ciclo*/
 
 
-// Controlliamo che non ci siano due numeri uguali nelle bombe 
+
+/TO DO -  CONTROLLARE COSA PUO DIVENTARE UNA FUNZIONE/ 
+
+// Controlliamo che non ci siano due numeri uguali nelle bombe. Se così non fosse, pushiamo e aggiungiamo all'array; 
 while (bombs.length < bombsNumbers) {
-    var cpuRandom = randomNumber (1, maxNumbers) 
+    var cpuRandom = randomNumber (1, maxNumbers); 
     if (!bombs.includes(cpuRandom)) {
         bombs.push(cpuRandom);
     }
 }
-// Includes va eseguito più volte, si potrebbe fare una funzione? 
 
-// Funzione che permette di calcolare numeri random da 1 a 100 
+// Funzione che permette di calcolare numeri random da 1 a 100; 
 function randomNumber (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
 console.log(bombs);
 
 
-/*6. Dobbiamo chiedere un numero all'utente */
-// 7. Controlliamo che questo numero sia un numero, che vada dal min al max e che l'utente non lasci il campo vuoto 
+// Controlliamo che vengano inseriti dei numeri, che non siano spazi, che non sia null e che i valori vadano da min a max;
+// Controlliamo inoltre se la scelta dell'utente è presente nell'array bombe e nell'array dei numeri già scelti;
+// Se la scelta utente passa i controlli, pushiamo e memorizziamo la scelta dell'utente per nuovi confronti;
 
-
-while (!isGameOver && userNumbers < userChance) { /*controlliamo che i numeri dell'utente siano tanti quanti i tentativi */
+while (!isGameOver && userNumbers.length < userChance) {
     do {
-        userChoice = parseInt(prompt("Inserisci un numero fino a " +maxNumbers))
-    } while (!userChoice /*controlliamo no null ecc */ || !isNaN(userChoice) /*controlliamo che sia un numero*/ || userChoice < 1 || userChoice > maxNumbers /*controlliamo max e min*/) 
-
-} 
-
-/* ok */
-
-
-/* Controlliamo che il numero scelto dall'utente non sia nelle bombe*/
-
-if(bombs.includes(userChoice)) {
-    console.log("Hai perso") 
-} else {
+        var userChoice = parseInt(prompt("Inserisci un numero"));
+    } while (!userChoice || isNaN(userChoice) || userChoice.trim() === " " || userChoice < 1 || userChoice > maxNumbers);  
+    /* si può fare funzione per validare numero */
     
+    if(bombs.includes(userChoice)) {
+        isGameOver = true;
+        // break 
+        //possiamo interrompere il ciclo con break o con un valore booleano;
+    } else {
+        if(userNumbers.includes(userChoice)) {
+            console.log("Hai già scelto questo numero! Scegline un altro.")
+        } else {
+            userNumbers.push(userChoice);
+        }
+    } 
+    console.log(userNumbers)
 }
+
+
+// Facciamo sapere all'utente se ha vinto o perso;
+if(isGameOver) {
+    console.log("Hai perso! Il tuo punteggio è di " + userNumbers.length);
+} else {
+    console.log("Complimenti, hai vinto!");
+}
+
